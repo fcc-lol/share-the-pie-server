dd84f69a2ffe49c406e2abdf350908a5346cd304
Leo Mancini <mancini@leo.gd>
2024-01-16 17:17:51 -0500

    Testing deploy

--- api-server.js
+++ api-server.js
@@ -93,7 +93,7 @@ app.post('/getReceiptData', async (req, res) => {
 })
 
 app.get('/status', async (req, res) => {
-  res.send('Success')
+  res.send('Test 123')
 })
 
 app.post('/parseReceiptImage', async (req, res) => {
